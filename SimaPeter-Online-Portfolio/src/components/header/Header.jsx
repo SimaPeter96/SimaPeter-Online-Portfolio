89fb3aff22e554b8dc0ced246807131ba022b5d8
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-08-18 14:58:40 +0200

    made new changes on the header and added a logo

--- SimaPeter-Online-Portfolio/src/components/header/Header.jsx
+++ SimaPeter-Online-Portfolio/src/components/header/Header.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Navbar from 'react-bootstrap/Navbar'; 
+import logo from '../Img/Sima-Logo.png';
 
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="logo"/>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
-          <Nav.Link href="#link">Skills</Nav.Link>
-          <Nav.Link href="#link">Projects</Nav.Link>
-          <Nav.Link href="#link">Contact</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>

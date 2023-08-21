423517e08eec16417fcdb5796472e60763e37e4d
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-08-21 15:23:22 +0200

    I made changes on all my sections. I added content on the sections

--- SimaPeter-Online-Portfolio/src/components/home/Home.jsx
+++ SimaPeter-Online-Portfolio/src/components/home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Img from '../Img/landing-page-img.jpg';
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Container>
+        <section className="Home" >
+            <div className="Home-content">
+                <h3>Hello, I'm</h3>
+                <h1>Sima Peter</h1>
+                <p>A junior software developer  
+                   I am a junior software developer based in Cape Town, 
+                   currently building websites using front-end and back-end.</p>
+                   <Button href="#">Download CV</Button>
+                   
+
+
+            </div>
+        </section>
+    </Container>
   )
 }
 

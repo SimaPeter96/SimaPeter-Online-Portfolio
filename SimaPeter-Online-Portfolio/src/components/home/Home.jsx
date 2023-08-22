784133d9b3830a7ea279fde6d6ef7835d9d477bc
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-08-22 12:25:32 +0200

    Changes have been made, resized images and added them to their folders.

--- SimaPeter-Online-Portfolio/src/components/home/Home.jsx
+++ SimaPeter-Online-Portfolio/src/components/home/Home.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Img from '../Img/landing-page-img.jpg';
+import Image from '../Img/Home.jpg';
+
 
 
 function Home() {
   return (
     <Container>
         <section className="Home" >
-            <div className="Home-content">
-                <h3>Hello, I'm</h3>
-                <h1>Sima Peter</h1>
-                <p>A junior software developer  
-                   I am a junior software developer based in Cape Town, 
-                   currently building websites using front-end and back-end.</p>
-                   <Button href="#">Download CV</Button>
-                   
-
-
-            </div>
+          <div style={{
+            width: '1920px',
+            height: '1080px',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(${Image})`
+          }}></div>
+      
         </section>
     </Container>
   )

00fb7bb5445e8b589c9823f019fac240551b9368
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-08-31 15:44:07 +0200

    changed the fonts and changed the color fonts on all pages. installed other libraries like animate .css. changed the skills page content. added new images to the img folder

--- SimaPeter-Online-Portfolio/src/components/home/Home.jsx
+++ SimaPeter-Online-Portfolio/src/components/home/Home.jsx
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 import './Home.css';
 
 
+
 function Home() {
   return (
     <section className="home" id="home">
@@ -14,7 +15,7 @@ function Home() {
             <div className="home-content">
               <h1>Hello, I'm</h1>
               <h2 className="home-content h2">Sima Peter</h2>
-              <p>A Junior <span>Software Development</span><br></br>
+              <p>A Junior <span>Software Developer</span><br></br>
                 I am a junior software developer based in Cape Town,
                 currently building websites using front-end and back-end.
               </p>
--- SimaPeter-Online-Portfolio/src/components/skills/Skills.jsx
+++ SimaPeter-Online-Portfolio/src/components/skills/Skills.jsx
@@ -1,89 +1,58 @@
 import React from 'react';
 import { Container, Row, Col} from 'react-bootstrap';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import Image1 from '../Img/html.png';
-import Image2 from '../Img/css.png';
-import Image3 from '../Img/js.png';
-import Image4 from '../Img/python.png';
-import Image5 from '../Img/java.png';
-import Image6 from '../Img/nodeJs.png';
-import Image7 from '../Img/react.png';
+import Image from '../Img/skillsGIF.gif';
+import 'animate.css'
+import Image1 from '../Img/CSSicon.png';
+import Image2 from '../Img/HTMLicon.png';
+import Image3 from '../Img/JSicon.png';
+import Image4 from '../Img/javaIcon.png';
+import Image5 from '../Img/PyIcon.png';
+import Image6 from '../Img/reactIcon.png';
+import Image7 from  '../Img/NodeJSIcon.png';
 import './Skills.css';
-
-const responsive = {
-  superLargeDesktop: {
-    
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
-
 
 
 function Skills() {
+  useEffect(() => {
+    const images = document.querySelectorAll('.img-bounce');
+
+    images.forEach((image, index) => {
+      image.classList.add('animate__animated', 'animate__bounce', `animate__delay-${index + 1}s`);
+    });
+  }, []);
+
+
   return (
     <section className="skills" id="skills" >
       <Container className='skills-content-container'>
         <Row className="skills-Col">
           <Col>
             <h1 className='skills-heading'>Skills</h1>
-            <p>These are the skills I've gained during my learnership program</p>
-          <div className="skills-bx">
-            <Carousel responsive={responsive} infinite={true}   className="skills-slider">
-         
-              <div className="html-icon">
-                  <img src={Image1} alt="" className='skills-image'/>
-                  <h2>HTML</h2>
-              </div>
-              <div className="css-icon">
-                  <img src={Image2} alt="" className='skills-image'/>
-                  <h2>CSS</h2>
-              </div>
-              <div className="js-icon">
-                  <img src={Image3} alt="" className='skills-image'/>
-                  <h2>JS</h2>
-              </div>
-              <div className="python-icon">
-                  <img src={Image4} alt="" className='skills-image'/>
-                  <h2>PY</h2>
-              </div>
-              <div className="java-icon">
-                  <img src={Image5} alt="" className='skills-image'/>
-                  <h2>JAVA</h2>
-              </div>
-              <div className="nodeJs-icon">
-                  <img src={Image6} alt="" className='skills-image'/>
-                  <h2>Node Js</h2>
-              </div>
-              <div className="react-icon">
-                  <img src={Image7} alt="" className='skills-image' />
-                  <h2>React</h2>
-              </div>
-             
-            </Carousel>
-          </div>
+            <p>These are the skills I've gained during my learnership program and intership</p>
           </Col>
         </Row>
+        <Row>
+        <Col md={6} className='GIF'>
+            <img src={Image}  alt="Image"  className="img-gif"
+            />
+          </Col>
+          <Col md={6} className='skills-icons'>
+            <img src={Image1} alt="CSS" className="img-bounce" />
+            <img src={Image2} alt="HTML" className="img-bounce" />
+            <img src={Image3} alt="JavaScript" className="img-bounce" />
+            <img src={Image4} alt="Java" className="img-bounce" />
+            <img src={Image5} alt="Python" className="img-bounce" />
+            <img src={Image6} alt="React" className="img-bounce" />
+            <img src={Image7} alt="Node.js" className="img-bounce" />
+          </Col>
+          </Row>
       </Container>
     </section>
 
-   
-
+  
     
   )
 }
 
 export default Skills
+
